a1564b44b60c920c519af0f9cecc5a64690c7e80
Andy Hsu <i@nn.ci>
2025-03-21 23:18:41 +0800

    fix: update play_long field type to Int64OrFloat for better flexibility in handling audio/video duration

--- fs.go
+++ fs.go
@@ -43,34 +43,34 @@ type GetFilesReq struct {
 }
 
 type GetFilesResp_File struct {
-	Fid       string   `json:"fid"`  // 文件ID
-	Aid       string   `json:"aid"`  // 文件的状态，aid 的别名。1 正常，7 删除(回收站)，120 彻底删除
-	Pid       string   `json:"pid"`  // 父文件夹ID
-	Fc        string   `json:"fc"`   // 文件分类 0 文件夹 1 文件
-	Fn        string   `json:"fn"`   // 文件名
-	Fco       string   `json:"fco"`  // 文件夹封面
-	Ism       string   `json:"ism"`  // 是否星标，1：星标
-	Isp       int      `json:"isp"`  // 是否加密；1：加密
-	Pc        string   `json:"pc"`   // 文件提取码
-	Upt       int64    `json:"upt"`  // 修改时间
-	Uet       int64    `json:"uet"`  // 修改时间
-	UpPt      int64    `json:"uppt"` // 上传时间
-	Cm        int64    `json:"cm"`
-	FDesc     string   `json:"fdesc"`     // 文件备注
-	IsPl      int64    `json:"ispl"`      // 是否统计文件夹下视频时长开关
-	Fl        []string `json:"fl"`        // 文件标签
-	Sha1      string   `json:"sha1"`      // 文件sha1
-	FS        int64    `json:"fs"`        // 文件大小
-	Fta       string   `json:"fta"`       // 文件状态 0/2 未上传完成，1 已上传完成
-	Ico       string   `json:"ico"`       // 文件后缀名
-	Fatr      string   `json:"fatr"`      // 音频长度
-	IsV       int64    `json:"isv"`       // 是否视频文件
-	Def       int64    `json:"def"`       // 视频清晰度；1:标清 2:高清 3:超清 4:1080P 5:4k;100:原画
-	Def2      int64    `json:"def2"`      // 视频清晰度；1:标清 2:高清 3:超清 4:1080P 5:4k;100:原画
-	PlayLong  int64    `json:"play_long"` // 音视频时长
-	VImg      string   `json:"v_img"`
-	Thumbnail string   `json:"thumb"` // 图片缩略图
-	Uo        string   `json:"uo"`    // 原图地址
+	Fid       string                  `json:"fid"`  // 文件ID
+	Aid       string                  `json:"aid"`  // 文件的状态，aid 的别名。1 正常，7 删除(回收站)，120 彻底删除
+	Pid       string                  `json:"pid"`  // 父文件夹ID
+	Fc        string                  `json:"fc"`   // 文件分类 0 文件夹 1 文件
+	Fn        string                  `json:"fn"`   // 文件名
+	Fco       string                  `json:"fco"`  // 文件夹封面
+	Ism       string                  `json:"ism"`  // 是否星标，1：星标
+	Isp       int                     `json:"isp"`  // 是否加密；1：加密
+	Pc        string                  `json:"pc"`   // 文件提取码
+	Upt       int64                   `json:"upt"`  // 修改时间
+	Uet       int64                   `json:"uet"`  // 修改时间
+	UpPt      int64                   `json:"uppt"` // 上传时间
+	Cm        int64                   `json:"cm"`
+	FDesc     string                  `json:"fdesc"`     // 文件备注
+	IsPl      int64                   `json:"ispl"`      // 是否统计文件夹下视频时长开关
+	Fl        []string                `json:"fl"`        // 文件标签
+	Sha1      string                  `json:"sha1"`      // 文件sha1
+	FS        int64                   `json:"fs"`        // 文件大小
+	Fta       string                  `json:"fta"`       // 文件状态 0/2 未上传完成，1 已上传完成
+	Ico       string                  `json:"ico"`       // 文件后缀名
+	Fatr      string                  `json:"fatr"`      // 音频长度
+	IsV       int64                   `json:"isv"`       // 是否视频文件
+	Def       int64                   `json:"def"`       // 视频清晰度；1:标清 2:高清 3:超清 4:1080P 5:4k;100:原画
+	Def2      int64                   `json:"def2"`      // 视频清晰度；1:标清 2:高清 3:超清 4:1080P 5:4k;100:原画
+	PlayLong  json_types.Int64OrFloat `json:"play_long"` // 音视频时长
+	VImg      string                  `json:"v_img"`
+	Thumbnail string                  `json:"thumb"` // 图片缩略图
+	Uo        string                  `json:"uo"`    // 原图地址
 }
 
 type GetFilesResp struct {
