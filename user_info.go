9f36957c0751c8ac818e077a3b09939dce36607d
Andy Hsu <i@nn.ci>
2025-06-02 16:24:32 +0800

    refactor: replace json_types.Int64OrFloat with json.Number for size fields and remove unused json_types files

--- user_info.go
+++ user_info.go
@@ -2,14 +2,13 @@ package sdk
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
-
-	"github.com/xhofe/115-sdk-go/json_types"
 )
 
 type UserInfoResp_Size struct {
-	Size       json_types.Int64OrFloat `json:"size"`
-	SizeFormat string                  `json:"size_format"`
+	Size       json.Number `json:"size"`
+	SizeFormat string      `json:"size_format"`
 }
 
 type UserInfoResp struct {
